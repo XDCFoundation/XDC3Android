0add04437b2390fa44b8758e4a0b1c287cfb61b5
ayush_dhar <ayushd@leewayhertz.com>
2021-07-01 17:09:28 +0530

    Read operations integrated with demo app

--- app/src/main/java/xinfin/sdk/Details.java
+++ app/src/main/java/xinfin/sdk/Details.java
@@ -3,12 +3,122 @@ package xinfin.sdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigInteger;
 
 public class Details extends AppCompatActivity {
+
+    TextView xdc_address_value,name_value,symbol_value,decimals_value,total_supply_value,balance_off_value,
+            transfer_value,allowance_value,approve_value,transfer_from_value,increase_allowance_value,decrease_allowance_value;
+//    BigInteger allowance,decimal,totalSupply;
+    String symbol,name,balance,allowance,decimal,totalSupply,xdc_address;
+    String hex_to_dec,bal,supply;
+    BigInteger dec_bal,dec_supply;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        hex_to_dec = "1000000000000000000";
+
+        xdc_address_value = findViewById(R.id.xdc_address_value);
+        name_value = findViewById(R.id.name_value);
+        symbol_value = findViewById(R.id.symbol_value);
+        decimals_value = findViewById(R.id.decimals_value);
+        total_supply_value = findViewById(R.id.total_supply_value);
+        balance_off_value = findViewById(R.id.balance_off_value);
+        transfer_value = findViewById(R.id.transfer_value);
+        allowance_value = findViewById(R.id.allowance_value);
+        approve_value = findViewById(R.id.approve_value);
+        transfer_from_value = findViewById(R.id.transfer_from_value);
+        increase_allowance_value = findViewById(R.id.increase_allowance_value);
+        decrease_allowance_value = findViewById(R.id.decrease_allowance_value);
+
+        getValues();
+        assignValues();
+
+
+
+    }
+
+    public void getValues(){
+        xdc_address = getIntent().getStringExtra("TOKEN_ADDRESS");
+        name = getIntent().getStringExtra("NAME");
+        symbol = getIntent().getStringExtra("SYMBOL");
+        balance = getIntent().getStringExtra("BALANCE");
+        decimal = getIntent().getStringExtra("DECIMAL");
+        allowance = getIntent().getStringExtra("ALLOWANCE");
+        totalSupply = getIntent().getStringExtra("TOTAL_SUPPLY");
+    }
+
+    public void assignValues(){
+
+        xdc_address_value.setText(xdc_address.replace("0x","xdc"));
+
+        if (name_value != null){
+            name_value.setText(name);
+        }
+        else
+            name_value.setText("-");
+
+        if (symbol_value != null){
+            symbol_value.setText(symbol);
+        }
+        else
+            symbol_value.setText("-");
+
+        if (balance_off_value!= null){
+
+                if (balance != null){
+                    bal = balance;
+                    BigInteger a
+                            = new BigInteger(bal);
+                    BigInteger b
+                            = new BigInteger(hex_to_dec);
+
+                    // Using divide() method
+                    dec_bal = a.divide(b);
+                    balance_off_value.setText(dec_bal.toString());
+                }
+
+
+
+        }
+        else
+            balance_off_value.setText("-");
+
+        if (decimals_value != null){
+            decimals_value.setText(decimal);
+        }
+
+        else
+            decimals_value.setText("-");
+
+        if (allowance_value !=null){
+            allowance_value.setText(allowance);
+        }
+       else
+           allowance_value.setText("-");
+
+       if (total_supply_value != null){
+           if (totalSupply != null){
+               supply = totalSupply;
+               BigInteger a
+                       = new BigInteger(supply);
+               BigInteger b
+                       = new BigInteger(hex_to_dec);
+
+               // Using divide() method
+               dec_supply = a.divide(b);
+               total_supply_value.setText(dec_supply.toString());
+           }
+
+       }
+       else
+           total_supply_value.setText("-");
+
+
     }
 }
--- app/src/main/java/xinfin/sdk/MainActivity.java
+++ app/src/main/java/xinfin/sdk/MainActivity.java
@@ -7,21 +7,19 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 import xinfin.sdk.constants.AppConstants;
 import xinfin.sdk.contracts.src.main.java.org.web3j.contracts.eip20.generated.ERC20;
@@ -31,16 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
     private Button submit_button;
     Web3j web3;
-     String token_number;
+     String token_address,xdcAddress;
      String hex_to_dec;
     ERC20 javaToken;
+    BigInteger allowance,decimal,totalSupply,balance;
+    String symbol,name;
+    AutoCompleteTextView tokenAutoTV;
+    TextView enterXdcAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        token_number = "0xe21e6d2235845a1f21d2808888afd598e72357db";
+
+        token_address = "0x847aefb3d207e69749e970f8574743a4f388b6f2";
         hex_to_dec = "1000000000000000000";
+//        xdcAddress = token_address.replace("0x","xdc");
+
+        enterXdcAddress = findViewById(R.id.enter_xdc_address);
+        enterXdcAddress.setText(token_address.replace("0x","xdc"));
 
          web3 = Web3j.build(new HttpService("https://rpc.apothem.network/"));
         try {
@@ -64,23 +71,13 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
 
-        Credentials creds = org.web3j.crypto.Credentials.create(AppConstants.PRIVATE_KEY);
+//        createToken();
 
-        javaToken = null;
-        try {
-            javaToken = ERC20.load(token_number, web3, creds, new DefaultGasProvider());
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
 
-        getBalance();
-        getSymbol();
-        getTotalSupply();
-        getName();
-        getDecimal();
-        getAllowance();
-        getApprove();
-        getTransferFrom();
+
+        initUI();
+//        getApprove();
+//        getTransferFrom();
 
 
 
@@ -98,50 +95,106 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (tokenAutoTV.getText() != null && tokenAutoTV.getText().length()>0){
+                    token_address = tokenAutoTV.getText().toString();
+                    enterXdcAddress.setText("");
+                   token_address =  token_address.replace("xdc","0x");
+                   getTokenDetails();
+                }
+                else{
+//                    token_address = enterXdcAddress.getText().toString();
+                    getTokenDetails();
+                }
+
+
+
+                getBalance();
+                getSymbol();
+                getTotalSupply();
+                getName();
+                getDecimal();
+                getAllowance();
+
                 Intent intent = new Intent(MainActivity.this,Details.class);
+                if (balance != null){
+                    intent.putExtra("BALANCE", balance.toString());
+                }
+
+                if (symbol != null){
+                    intent.putExtra("SYMBOL", symbol);
+                }
+
+                if (totalSupply != null){
+                    intent.putExtra("TOTAL_SUPPLY", totalSupply.toString());
+                }
+
+                if (name != null){
+                    intent.putExtra("NAME", name);
+                }
+                token_address.replace("0x","xdc");
+                intent.putExtra("TOKEN_ADDRESS",token_address);
+                if (decimal != null){
+                    intent.putExtra("DECIMAL", decimal.toString());
+                }
+
+               if (allowance != null){
+                   intent.putExtra("ALLOWANCE", allowance.toString());
+               }
+
+
                 startActivity(intent);
 
             }
         });
 
-        initUI();
+
     }
 
+    public void getTokenDetails(){
+        Credentials creds = org.web3j.crypto.Credentials.create(AppConstants.PRIVATE_KEY);
+
+        javaToken = null;
+        try {
+            javaToken = ERC20.load(token_address, web3, creds, new DefaultGasProvider());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
 // to fetch allowance
     public void getAllowance(){
         try {
-            BigInteger allowance = javaToken.allowance(token_number,"0x2e550836caaa79884f36e78626363f59ca50e96e").send();
+            allowance = javaToken.allowance(token_address,"0x2e550836caaa79884f36e78626363f59ca50e96e").send();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
 
-    public void getApprove(){
+//    public void getApprove(){
+//
+//        try {
+//            TransactionReceipt approve = javaToken.approve("0x03c0d9bc556be68870b96976e81d32ebb49d335d",BigInteger.valueOf(1000)).send();
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//        }
+//
+//    }
 
-        try {
-            TransactionReceipt approve = javaToken.approve("0x03c0d9bc556be68870b96976e81d32ebb49d335d",BigInteger.valueOf(1000)).send();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-    }
-
-    public void getTransferFrom(){
-
-        try {
-            TransactionReceipt transfer = javaToken.transferFrom("0x03c0d9bc556be68870b96976e81d32ebb49d335d","0x114ac1863dcd99cdbe7b33eb69a87e12fb95f47a",BigInteger.valueOf(1000)).send();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-    }
+//    public void getTransferFrom(){
+//
+//        try {
+//            TransactionReceipt transfer = javaToken.transferFrom("0x03c0d9bc556be68870b96976e81d32ebb49d335d","0x114ac1863dcd99cdbe7b33eb69a87e12fb95f47a",BigInteger.valueOf(1000)).send();
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//        }
+//
+//    }
 
 // to fetch the decimals of the token
     public void getDecimal(){
 
         try {
-            BigInteger decimal = javaToken.decimals().send();
+            decimal = javaToken.decimals().send();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
@@ -152,7 +205,7 @@ public class MainActivity extends AppCompatActivity {
     public void getName(){
 
         try {
-            String name = javaToken.name().send();
+            name = javaToken.name().send();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
@@ -162,9 +215,9 @@ public class MainActivity extends AppCompatActivity {
 
 // to fetch he total supply of the token
     public void getTotalSupply(){
-        BigInteger totalSuppy = null;
+        totalSupply = null;
         try {
-            totalSuppy = javaToken.totalSupply().send();
+            totalSupply = javaToken.totalSupply().send();
 
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -176,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
     public void getSymbol(){
 
         try {
-            String symbol = javaToken.symbol().send();
+            symbol = javaToken.symbol().send();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
@@ -186,20 +239,26 @@ public class MainActivity extends AppCompatActivity {
 // to fetch the balance of the token
     public void getBalance()
     {
+
         try {
-            web3.ethGetBalance(token_number, DefaultBlockParameterName.LATEST).sendAsync().get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            balance = javaToken.balanceOf(token_address).send();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
+//        try {
+//            web3.ethGetBalance(token_number, DefaultBlockParameterName.LATEST).sendAsync().get();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 
 
     private void initUI()
     {
         //UI reference of textView
-        final AutoCompleteTextView customerAutoTV = findViewById(R.id.customerTextView);
+        tokenAutoTV = findViewById(R.id.customerTextView);
 
         // create list of customer
         ArrayList<String> customerList = getCustomerList();
@@ -208,14 +267,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, customerList);
 
         //Set adapter
-        customerAutoTV.setAdapter(adapter);
+        tokenAutoTV.setAdapter(adapter);
     }
 
     private ArrayList<String> getCustomerList()
     {
         ArrayList<String> address = new ArrayList<>();
-        address.add("0xd0A1E359811322d97991E03f863a0C30C2cF029C");
-        address.add("0xd0A1E359811322d97991E03f863a0C30C2cF029D");
+        address.add("xdc847aefb3d207e69749e970f8574743a4f388b6f2");
+        address.add("xdce7c09f7c38156eaff9864d5d2dc83723b804f927");
+        address.add("xdcce20035eceecd1f94ac4a828de5e3f9b7d2c7898");
         return address;
     }
 
