57e8f4bbd4f7c8e746b012780d9030e6fbee2a85
Ayush Dhar <ayushd@leewayhertz.com>
2021-07-22 14:08:42 +0530

    transfer

--- app/src/main/java/xinfin/sdk/CreateAccount.java
+++ app/src/main/java/xinfin/sdk/CreateAccount.java
@@ -43,6 +43,16 @@ public class CreateAccount extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        try {
+            approveERC20Token();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String transferERC20Token() throws ExecutionException, InterruptedException, IOException {
@@ -69,6 +79,50 @@ public class CreateAccount extends AppCompatActivity {
 //        value = value.multiply(VALUE);
         Function function = new Function(
                 "transfer",
+                Arrays.asList(new Address("0x73585ae0c1aa818db5f360ed734ffad68d9b2ef8"), new Uint256(BigInteger.valueOf(1000))),
+                Collections.singletonList(new TypeReference<Type>() {
+                }));
+        // Create RawTransaction transaction object
+        String encodedFunction = FunctionEncoder.encode(function);
+        RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, gasPrice, gasLimit,
+                "0xd3d1ea96362d2660d38c749c196370b5619a3620", encodedFunction);
+
+        //Signature Transaction
+        byte[] signMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signMessage);
+        //Send the transaction
+        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
+        String hash = ethSendTransaction.getTransactionHash();
+        if (hash != null) {
+            return hash;
+        }
+        return null;
+    }
+
+    public static String approveERC20Token() throws ExecutionException, InterruptedException, IOException {
+        Web3j web3j = Web3j.build(new HttpService(AppConstants.BASE_URL));
+        // Load the required documents for the transfer, with the private key
+        Credentials credentials = Credentials.create("0xe258e6b92739236070ca1baf6ee4ae63203237fec29249a96d122f24f18f7ded");
+        // Get nonce, the number of transactions
+        BigInteger nonce;
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount("0x32f158af29c171392a1ff35a7387583ff4959053", DefaultBlockParameterName.LATEST).sendAsync().get();
+        if (ethGetTransactionCount == null) {
+            return null;
+        }
+        nonce = ethGetTransactionCount.getTransactionCount();
+        //gasPrice and gasLimit can be set manually
+        BigInteger gasPrice;
+        EthGasPrice ethGasPrice = web3j.ethGasPrice().sendAsync().get();
+        if (ethGasPrice == null) {
+            return null;
+        }
+        gasPrice = ethGasPrice.getGasPrice();
+        //BigInteger.valueOf(4300000L) If the transaction fails, it is probably a problem with the setting of the fee.
+        BigInteger gasLimit = BigInteger.valueOf(60000L);
+        //ERC20 token contract method
+//        value = value.multiply(VALUE);
+        Function function = new Function(
+                "approve",
                 Arrays.asList(new Address("0x73585ae0c1aa818db5f360ed734ffad68d9b2ef8"), new Uint256(BigInteger.valueOf(1000))),
                 Collections.singletonList(new TypeReference<Type>() {
                 }));
