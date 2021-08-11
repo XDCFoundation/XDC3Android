243cb1e432f31d651ae02a2fd1d362c941811a6f
Ayush Dhar <ayushd@leewayhertz.com>
2021-08-11 19:31:41 +0530

    Config file for test cases

---
+++ Xinfin_Sdk_Android/XDC3Android/src/main/java/xinfin/XDCAndroid/ConfigXDC721.java
@@ -0,0 +1,22 @@
+package xinfin.XDCAndroid;
+
+public class ConfigXDC721 {
+
+    public static final String TOKEN_ADDRESS = "0x5f07d8ca1f176ec49b738cfe309db9a9cf369c88";
+
+    public static final String OWNER_ADDRESS = "0x7af2f691d78c51d0f0368a3c367634c9d76cfe7d";
+
+    public static final String OWNER_PRIVATE_KEY = "0x78e5eff6026eeeee03401909c51a1b3bd3b2071dd333632bb4d54c279dc5f1ea";
+
+    public static final String SPENDER_ADDRESS = "0x79dbbbe40993253aa18e930a39d3636b15866725";
+
+    public static final String SPENDER_PRIVATE_KEY = "0x03098465d0a7a39e05ca8ab431789507c1d576a2da1de6934964e33ea8d6986a";
+
+    public static final String TOKEN_ID = "25";
+
+    public static final String INTERFACE_ID = "0x80ac58cd";
+
+    public static final String TOKEN_READ = "0xf6b4102b25756a33a90f8f192c74e53d22a9628e";
+
+    public static final String TOKEN_INDEX = "0";
+}
