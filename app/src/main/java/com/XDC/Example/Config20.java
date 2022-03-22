b0abc9633517913fb4619489886f53a9ebb690aa
Ayushd_AyushDhar <ayushd@leewayhertz.com>
2022-03-22 11:57:08 +0530

    minor changes

--- app/src/main/java/com/XDC/Example/Config20.java
+++ app/src/main/java/com/XDC/Example/Config20.java
@@ -4,7 +4,7 @@ public class Config20
 {
 
 
-        public static final String TOKEN_ADDRESS = "0xba9d6a36fbc194f5d1aa48a2892ae4bdf6939cb9";
+        public static final String TOKEN_ADDRESS = "0x053F814deb5F4968707bF1D55b75d43e88389B3D";
 
         public static final String OWNER_ADDRESS = "0x6ffe09f9302a857fcb122296e3ab3bb80c45cbcd";
 
@@ -19,6 +19,21 @@ public class Config20
         public static final String TRANSFER_FROM_ADDRESS = "0xedb472070566e072f3bdaa50cfa076e772135f33";
 
 
+//        echo dash average autumn devote shallow card outside smart bulk arm fluid
+//        0x8f64eea8a4cee0db99b14eadf5e096cfc9a4c01d // account address
+//        : c00f62a85aea1768cbf1d7bffab4bf102fe7507f1a8d52ec71d490b662e5966a // private key
+
+//        xdc053F814deb5F4968707bF1D55b75d43e88389B3D - MTK
+//        xdcFeE1C7bdA8103dE146e35F9204252E8cC0ff0004 - ERC721
 
 
+        // 0x8b462b7c7142c5db99dedf6b8b4406b947ed93fd
+//        613f995586518c59e239658cd418d705c81e7afd4a51f65b5c56cb29f3c86afd
+//        toe page hill swarm exist original author tower truth photo casino survey
+//        da45df6da879c7a746f62ee95cbc73a1001b47737d0075a662f326c6031ff77d
+
+//        xdcbbcea945d74b659ec37afd49af746d9ccda1530b - to address
+
+
+//        0xDa2B731d88b556e1Dbad396a187D5949D9f4e472 - token address
 }
